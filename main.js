173075512e3f5d4a6d39aaf9b0eb387c96d5f3e3
TomVrankenOAF <54804828+TomVrankenOAF@users.noreply.github.com>
2019-12-11 18:15:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1186,7 +1186,7 @@ var HospitalRegionText = function(){
     else {sayText("Tafadhali chagua mkoa wako:\n1) Central\n2) Coast\n3) Eastern\n4) Nairobi\n5) North Eastern\n6) Nyanza\n7) Rift Valley\n8) Western")}
 };
 // Start logic flow
-function global.main() {
+global.main = function () {
     LogSessionID();
     SplashMenuText();
     promptDigits("SplashMenu", {submitOnHash: true, maxDigits: 8, timeout: 5});
