43598b37f4174ece79f9a99fa8ab5fe2b9f795c5
TomVrankenOAF <54804828+TomVrankenOAF@users.noreply.github.com>
2020-03-23 09:01:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -732,6 +732,7 @@ var CallBackCreate = function(client,phonenumberCB,type){
     var Subject = "Call back requested for: "+type+" accountnumber :"+ client.AccountNumber;
     var Body = "call back number: "+ phonenumberCB+ "\n issuetype: "+ type+ "\nFull name: "+client.ClientName+"\n Account number: "+ client.AccountNumber+ "\nDistrict: "+ client.DistrictName+ "\nSite: "+ client.SiteName+ "\nGroup: "+ client.GroupName;
     sendEmail(CEEmail, Subject, Body);
+    sendEmail("global.business.operations@oneacrefund.org", Subject, Body);
 };
 
 var StaffCallBackCreate = function(phonenumberCB,type,Body){
